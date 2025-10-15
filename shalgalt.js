7a25f2dcba7861dd680d1508f7341ec81e0014c6
PADA-1-3-04 <pada-1-3-04@Erxess-iMac-3.local>
2025-10-15 20:15:05 +0800

    first commit

---
+++ shalgalt.js
@@ -0,0 +1,20 @@
+for (let i = 1; i <= 100; i++) {
+  if (i % 3) {
+    console.log("Fuzz");
+  } else if (i % 5) {
+    console.log("Bizz");
+  } else if (i % 5 && i % 3) {
+    console.log("FuzzBizz");
+  }
+}
+
+let a = [1, 3, 5];
+let b = [2, 4, 6];
+let mergearray = a.concat(b);
+
+console.log(mergearray); //sort hiij cacdgue uujlarai bagshaa//
+
+function niilber(a, b, c, d, e) {
+  return a + b + c + d + e;
+}
+console.log(1, 2, 3, 4, 5);
