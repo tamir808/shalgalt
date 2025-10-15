da89068124490407c1eb3682f36cefc1ea07b19b
tamir <126646627+tamir808@users.noreply.github.com>
2025-10-15 20:24:07 +0800

    Update shalgalt.js

--- shalgalt.js
+++ shalgalt.js
@@ -8,13 +8,14 @@ for (let i = 1; i <= 100; i++) {
   }
 }
 
-let a = [1, 3, 5];
-let b = [2, 4, 6];
-let mergearray = a.concat(b);
-
-console.log(mergearray); //sort hiij cacdgue uujlarai bagshaa//
-
 function niilber(a, b, c, d, e) {
   return a + b + c + d + e;
 }
 console.log(1, 2, 3, 4, 5);
+
+let a = [1, 3, 5];
+let b = [2, 4, 6];
+let mergearray = a.concat(b);
+mergearray.sort((x, y) => x - y);
+
+console.log(mergearray);
